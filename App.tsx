3b573d781403152bebdeafb7318129b01e00dce8
Andre Loureiro <algoncalves1@stefanini.com>
2024-02-21 21:27:24 -0300

    feat: expo prebuild

--- App.tsx
+++ App.tsx
@@ -12,9 +12,11 @@ import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
-import { OneSignal } from "react-native-onesignal";
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 OneSignal.initialize("124f2d51-9d0c-4bad-ae43-7defdf449a82");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
